9b1a53874bff7d57988d3b04fe0bd61835aced31
afb-dev-2026 <65ksr65wn4@privaterelay.appleid.com>
2026-05-02 21:47:33 +0800

    Add configuration file for WhatsApp and email settings

---
+++ config/config.js
@@ -0,0 +1,14 @@
+export const CONFIG = {
+  whatsappNumber: "60123456789",
+
+  emailJS: {
+    serviceID: "YOUR_SERVICE_ID",
+    templateID: "YOUR_TEMPLATE_ID",
+    publicKey: "YOUR_PUBLIC_KEY"
+  },
+
+  emails: [
+    "jomshuttle@gmail.com",
+    "fahmie1997@yahoo.com"
+  ]
+};
